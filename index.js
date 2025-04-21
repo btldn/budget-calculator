895abc5a024110838376f77548686fe0a684f46d
btldn <darkzell20@gmail.com>
2025-04-21 16:21:09 +0300

    feat: work on draw transactions

--- index.js
+++ index.js
@@ -1,30 +1,113 @@
-import addTransactionRequest from "./modules/api.js"
+import { addTransactionRequest, arrOfTransactions } from "./modules/api.js"
 
 const inputSource = document.querySelector('.budget_input-source')
 const inputCategory = document.querySelector('.budget_input-category')
 const inputAmount = document.querySelector('.budget_input-amount')
 const inputCommentary = document.querySelector('.budget_input-commentary')
+const inputDate = document.querySelector('.budget_input-date')
 
 const addTransactionButton = document.querySelector('.budget_inputs-add')
 
 const historyList = document.querySelector('.budget_list')
+
+const dayAmount = document.querySelectorAll('.budget_item-amount')
+
+function datesOfTransactions() {  
+    const dates = []
+    for (let transaction of arrOfTransactions) {
+        dates.push(transaction.date)
+    }
+    return dates
+}
+
 
 function handleAddTransaction() {
     const transaction = {
         category: inputCategory.value,
         source: inputSource.value,
         amount: +inputAmount.value,
-        commentary: inputCommentary.value
+        commentary: inputCommentary.value,
+        date: inputDate.value
     }
 
-    const response = addTransactionRequest(transaction)
-
-    console.log(3132)
+    const response = addTransactionRequest(transaction.date, transaction)
 
     inputAmount.value = ''
     inputCommentary.value = ''
-
 }
+
+function createDate(date) {
+    const dateWrapper = document.createElement('div')
+    dateWrapper.className = 'budget_item-wrapper'
+    historyList.append(dateWrapper)
+
+    const newDate = document.createElement('div')
+    newDate.className = 'budget_item-header'
+    dateWrapper.append(newDate)
+
+    const transactionDate = document.createElement('h3')
+    transactionDate.className = 'budget_item-date'
+    transactionDate.innerHTML = date
+    newDate.append(transactionDate)
+
+    const daySum = document.createElement('span')
+    daySum.className = 'budget_item-date-sum'
+    daySum.innerHTML = 0
+    newDate.append(daySum)
+ 
+}
+
+export function createTransaction(transaction) {
+    const dateWrapper = document.querySelector('.budget_item-wrapper') 
+
+    const newTransaction = document.createElement('li')
+    newTransaction.className = 'budget_item'
+    newTransaction.dataset.id = transaction.id
+    dateWrapper.append(newTransaction)
+
+    const transactionTitle = document.createElement('div')
+    transactionTitle.className = 'budget_item-title'
+    newTransaction.append(transactionTitle)
+
+    const transactionCategory = document.createElement('span')
+    transactionCategory.className = 'budget_item-category'
+    transactionCategory.innerHTML = transaction.category
+    transactionTitle.append(transactionCategory)
+
+    const transactionSource = document.createElement('span')
+    transactionSource.className = 'budget_item-source'
+    transactionSource.innerHTML = transaction.source
+    transactionTitle.append(transactionSource)
+
+    const transactionAmount = document.createElement('div')
+    transactionAmount.className = 'budget_item-amount'
+    transactionAmount.innerHTML = `${transaction.amount} ₽`
+    if (transaction.amount >= 0) {
+        transactionAmount.classList.add('positive-amount')
+    } else {
+        transactionAmount.classList.add('negative-amount')
+    }
+    newTransaction.append(transactionAmount)
+}
+
+function renderTransaction() {
+    const daySum = document.querySelector('.budget_item-date-sum')
+    const datesWrapper = document.querySelectorAll('.budget_item-wrapper')
+    const dates = datesOfTransactions()
+    let daySumAmount = 0
+
+    for (let date of arrOfTransactions) {
+        createDate(date)
+    }
+
+    // console.log(arrOfTransactions)
+    // console.log(daySumAmount)
+    daySum.innerHTML = `${daySumAmount} ₽`
+    
+}
+
+renderTransaction()
+
 
 addTransactionButton.addEventListener('click', handleAddTransaction)
 
--- modules/api.js
+++ modules/api.js
@@ -1,8 +1,32 @@
 import { createTransaction } from "../index.js"
 
+export const arrOfTransactions = new Map()
+
 const historyUrl = 'https://680256170a99cb7408e950d7.mockapi.io/transactions'
 
-export default async function addTransactionRequest(transaction) {
+export default async function getTransactions() {
+    try {
+        const response = await fetch(historyUrl)
+        const transactions = await response.json()
+
+        for (let transaction of transactions) {
+            if (arrOfTransactions.has(transaction.date)) {
+                arrOfTransactions.get(transaction.date).push(transaction)
+            } else {
+                arrOfTransactions.set(transaction.date, [transaction])
+            }
+        } 
+        console.log(arrOfTransactions)
+
+    } catch (error) {
+
+    }
+    
+}
+
+await getTransactions() 
+
+export async function addTransactionRequest(date, transaction) {
     try {
         const response = await fetch(historyUrl, {
             method: 'POST',
@@ -12,11 +36,13 @@ export default async function addTransactionRequest(transaction) {
             body: JSON.stringify(transaction)
         })
 
-        createTransaction(transaction)
+        console.log(date, transaction)
+        createDate(date, transaction)
+        
+
         return response
         
     } catch (error) {
 
-    
     }
 }
